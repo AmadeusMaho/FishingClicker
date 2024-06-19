b6af297b831fa33ae7ee8eb8a84efdbfa5b256e6
Mauri <maur.gatica@duocuc.cl>
2024-06-19 02:30:56 -0400

    audio, cambio de pez aun por definir, cambio variables para más legibilidad

--- main.lua
+++ main.lua
@@ -1,41 +1,60 @@
 --import anim8
+-- explicación de anim8 --> 
+-- añade un nuevo spritesheet (varios sprites juntos para una animación) revisar imagen por si hay dudas
+-- crea un grid a partir del spritesheet (básicamente, el tamaño de cada sprite, los primeros 2 parámetros es x e y)
+-- luego el tercer y cuarto parámetro es el tamaño total de la hoja, recibe el ancho y el alto del spritesheet
+-- crea la animación, '1-8' significa que ocupa el sprite 1 al 8, el 1 significa que es de
+-- la columna 1 y el 0.2 es la velocidad a la que cambia la imagen
 anim8 = require 'libraries.anim8'
+
 
 --musica
 music = {}
 music.level1 = love.audio.newSource("audio/m1.mp3","static")
+
+-- sonidos
+sonidos = {}
+sonidos.bg = love.audio.newSource("audio/bg-sound.mp3","static")
+sonidos.click = love.audio.newSource("audio/sfx.mp3","static")
+
 --backgrounds
 bg = {}
 bg.level1 = love.graphics.newImage("resources/bg.jpg")
---pescados
-pescado = {}
--- explicación de anim8 --> 
--- añade un nuevo spritesheet (varios sprites juntos para una animación) revisar imagen por si hay dudas
-pescado.PnaranjoSpritesheet = love.graphics.newImage('sprites/pescadoOrange.png')
--- crea un grid a partir del spritesheet (básicamente, el tamaño de cada sprite, los primeros 2 parámetros es x e y)
--- luego el tercer y cuarto parámetro es el tamaño total de la hoja, recibe el ancho y el alto del spritesheet
-pescado.PnaranjoGrid = anim8.newGrid(16,16,pescado.PnaranjoSpritesheet:getWidth(),pescado.PnaranjoSpritesheet:getHeight())
+
+--pez
+peces = {}
+peces.lvl1Spritesheet = love.graphics.newImage('sprites/pez1.png')
+peces.lvl1Grid = anim8.newGrid(16,16,peces.lvl1Spritesheet:getWidth(),peces.lvl1Spritesheet:getHeight())
 animaciones = {}
--- crea la animación, '1-8' significa que ocupa el sprite 1 al 8, el 1 significa que es de
--- la columna 1 y el 0.2 es la velocidad a la que cambia la imagen
-animaciones.Pnaranjo = anim8.newAnimation(pescado.PnaranjoGrid('1-8',1),0.2)
+animaciones.pezlvl1 = anim8.newAnimation(peces.lvl1Grid('1-4',1),0.2)
 
 --tamaño pantalla
 ancho,alto = love.graphics.getDimensions()
+
 function love.load()
+    --quita lo borroso
+    love.graphics.setDefaultFilter("nearest","nearest")
+    love.audio.play(sonidos.bg)
+    sonidos.bg:setVolume(0.4)
+    sonidos.click:setVolume(0.7)
     love.audio.play(music.level1)
-    love.window.setTitle("Clicker V2")
+    love.window.setTitle("Fish")
     cursor = love.graphics.newImage("resources/Fish-rod.png")
 end
 
 function love.update(dt)
-    animaciones.Pnaranjo:update(dt)
+    animaciones.pezlvl1:update(dt)
 end
 
 function love.draw()
+    love.graphics.setNewFont(30)
+    mouse = {}
+    mouse.X,mouse.Y = love.mouse.getPosition()
+    love.graphics.print(mouse.X..mouse.Y)
     love.graphics.draw(bg.level1,0,0,0,nil)
-    animaciones.Pnaranjo:draw(pescado.PnaranjoSpritesheet,ancho/2,alto/2,nil,2,2)
+    animaciones.pezlvl1:draw(peces.lvl1Spritesheet,ancho/2,alto/2,nil,6,6)
 end
 
 function love.mousepressed()
+    love.audio.play(sonidos.click)
 end

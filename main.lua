0a3c25348093bc1cdba34f9c4f77951d3dcaf73f
Mauri <maur.gatica@duocuc.cl>
2024-06-20 12:00:54 -0400

    número para peces, una compra programada

--- main.lua
+++ main.lua
@@ -24,6 +24,7 @@ sonidos = {}
 sonidos.bg = love.audio.newSource("audio/bg-sound.mp3","static")
 sonidos.click = love.audio.newSource("audio/sfx.mp3","static")
 sonidos.sell = love.audio.newSource("audio/sellsound.mp3","static")
+sonidos.upgrade1 = love.audio.newSource("audio/upgrade1.mp3","static")
 
 --backgrounds
 bg = {}
@@ -39,13 +40,16 @@ peces.lvl1.sx, peces.lvl1.sy = peces.lvl1.sprite:getDimensions()
 peces.lvl1.escala = 1
 peces.lvl1.targetScale = 1 
 peces.lvl1.scaleSpeed = 2 
--- Tiempo de agrandamiento del pez
+-- tiempo de agrandamiento del pez
 scaleTime = 0 -- tiempo restante para que el pez permanezca agrandado
 scaleDuration = 0.2 -- duración en segundos del agrandamiento
 
 
 -- mejoras
 mejoras = {}
+mejoras.cania = {}
+mejoras.cania.proxMejora = 15
+mejoras.cania.nivelMejora = 0
 
 
 --botones
@@ -56,13 +60,17 @@ botones.vender = love.graphics.newImage("resources/botones/vender.png")
 -- acumulador peces
 clickPeces = {}
 clickPeces.cant = 0
+clickPeces.numberTime = 0;clickPeces.numberSize = 0
+clickPeces.cantPerClick = 1
+clickPeces.posRandomX = math.random(50)
+clickPeces.posRandomY = math.random(70)
 
 -- monedas
 monedaSpritesheet = love.graphics.newImage("resources/moneda.png")
 monedaGrid = anim8.newGrid(16,16,monedaSpritesheet:getWidth(),monedaSpritesheet:getHeight())
 animaciones = {}
 animaciones.moneda = anim8.newAnimation(monedaGrid('1-7',1),0.2)
-monedaCant = 0
+monedaCant = 0;multMonedas=1
 
 
 function love.load()
@@ -71,6 +79,7 @@ function love.load()
     love.audio.play(sonidos.bg)
     sonidos.bg:setVolume(0.4)
     sonidos.click:setVolume(0.7)
+    sonidos.sell:setVolume(0.5)
     love.audio.play(music.level1)
     love.window.setTitle("Fish")
     cursor = love.mouse.newCursor("resources/cursor.png")
@@ -80,6 +89,13 @@ end
 function love.update(dt)
     --animaciones.pezlvl1:update(dt)
     animaciones.moneda:update(dt)
+
+    if clickPeces.numberTime > 0 then
+        clickPeces.numberTime = clickPeces.numberTime - dt
+        clickPeces.numberSize = 1
+    else
+        clickPeces.numberSize = 0
+    end
 
     if scaleTime > 0 then
         scaleTime = scaleTime - dt
@@ -99,11 +115,17 @@ function love.draw()
     animaciones.moneda:draw(monedaSpritesheet,1100,30,nil,1.5,1.5)
     love.graphics.setNewFont(20)
     love.graphics.print(monedaCant,1130,30 )
-    font = love.graphics.newFont("resources/font.ttf",50)
+    font = love.graphics.newFont("resources/font.ttf",52)
+    fontPequenia = love.graphics.newFont("resources/font.ttf",23)
     love.graphics.setFont(font)
-    love.graphics.print("Peces: "..clickPeces.cant,ancho/2-120,alto/2-110) -- cantidad peces
+    love.graphics.print("Peces: "..clickPeces.cant,ancho/2-90,alto/2-110)
+    love.graphics.setFont(fontPequenia)
+    love.graphics.print("Nivel: "..mejoras.cania.nivelMejora,20,165)
+    love.graphics.print("Próxima mejora: "..mejoras.cania.proxMejora.." oro",20,265) 
     love.graphics.print(mouseX..mouseY)
     love.graphics.draw(peces.lvl1.sprite,ancho/2-150,alto/2-100,nil,peces.lvl1.escala ,peces.lvl1.escala)
+    love.graphics.setFont(font)
+    love.graphics.print("+"..clickPeces.cantPerClick ,ancho/2+clickPeces.posRandomX,alto/2+clickPeces.posRandomY,nil,clickPeces.numberSize,clickPeces.numberSize)  
     love.graphics.draw(botones.vender,1080,60,nil,0.7,0.7)
 
 end
@@ -113,18 +135,32 @@ function love.mousepressed()
     mouseY >= alto/2-10 and mouseY <= alto/2-10 + peces.lvl1.sy-60 then
         isClicked = true
         love.audio.play(sonidos.click)
-        clickPeces.cant = clickPeces.cant + 1
+        clickPeces.cant = clickPeces.cant + clickPeces.cantPerClick
         scaleTime = scaleDuration
         peces.lvl1.escala = 1.2
+        clickPeces.numberTime = 0.5
+        clickPeces.posRandomX = math.random(50)
+        clickPeces.posRandomY = math.random(70)
+        
     else
         peces.lvl1.escala = 1
     end
 
      if mouseX >= 1080 and mouseX <= 1080 + botones.vender:getWidth() and
-    mouseY >= 60 and mouseY <= 60 + botones.vender:getHeight() then
+    mouseY >= 60 and mouseY <= 60 + botones.vender:getHeight() and clickPeces.cant > 0 then
         love.audio.play(sonidos.sell) 
-        monedaCant = monedaCant + (clickPeces.cant * 2)
+        monedaCant = monedaCant + (clickPeces.cant * multMonedas)
         clickPeces.cant = 0
+    end
+
+    if mouseX >= 10 and mouseX <= 10 + botones.cania:getWidth() and
+    mouseY >= 200 and mouseY <= 200 + botones.cania:getHeight() and monedaCant > mejoras.cania.proxMejora then
+        --clickPeces.cant = clickPeces.cant - mejoras.cania.proxMejora
+        love.audio.play(sonidos.upgrade1)
+        clickPeces.cantPerClick = clickPeces.cantPerClick + 1
+        monedaCant = monedaCant - mejoras.cania.proxMejora
+        mejoras.cania.proxMejora = mejoras.cania.proxMejora + 20
+        mejoras.cania.nivelMejora = mejoras.cania.nivelMejora + 1       
     end
 
 
